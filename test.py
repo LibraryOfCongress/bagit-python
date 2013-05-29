5188e836448049ed60d864902856c9d9561604e6
Joby Harding <jobyharding@gmail.com>
2013-05-29 17:14:38 +0100

    remove debug code

--- test.py
+++ test.py
@@ -132,6 +132,8 @@ class TestBag(unittest.TestCase):
         bf = open(os.path.join(self.tmpdir, "bagit.txt"), "wb")
         bf.write(bagfile)
         bf.close()
+        import pdb
+        pdb.set_trace()
         bag = bagit.Bag(self.tmpdir)
         self.assertRaises(bagit.BagValidationError, bag.validate)
 
