21dc13c775c31b34b593f6e2125db52d5cc6112b
Ed Summers <ehs@pobox.com>
2014-05-09 09:33:29 -0400

    changed project url

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except:
 setup(
     name = 'bagit',
     version = '1.3.7',
-    url = 'http://github.com/LibraryOfCongress/bagit-python',
+    url = 'https://libraryofcongress.github.io/bagit-python/'
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
     py_modules = ['bagit',],
