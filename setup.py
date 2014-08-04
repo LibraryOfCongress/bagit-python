19de13282f9a495986dfe24e7bde990bb8ff995a
Ed Summers <ehs@pobox.com>
2014-08-04 04:38:14 -0400

    upped version for release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ except:
 
 setup(
     name = 'bagit',
-    version = '1.5.0',
+    version = '1.5.1',
     url = 'https://libraryofcongress.github.io/bagit-python/',
     author = 'Ed Summers',
     author_email = 'ehs@pobox.com',
