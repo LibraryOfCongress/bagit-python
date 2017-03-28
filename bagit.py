69b568c2e42074a241410e809e484f9dd47ffba7
Chris Adams <cadams@loc.gov>
2017-03-28 16:22:43 -0400

    More prose cleanup

--- bagit.py
+++ bagit.py
@@ -8,27 +8,36 @@ can be found at:
     http://purl.org/net/bagit
 
 bagit.py is a pure python drop in library and command line tool for creating,
-and working with BagIt directories:
+and working with BagIt directories.
 
-    import bagit
-    bag = bagit.make_bag('example-directory', {'Contact-Name': 'Ed Summers'})
-    print bag.entries
 
-Basic usage is to give bag a directory to bag up:
+Command-Line Usage:
 
-    % bagit.py my_directory
+Basic usage is to give bagit.py a directory to bag up:
+
+    $ bagit.py my_directory
 
 This does a bag-in-place operation where the current contents will be moved
 into the appropriate BagIt structure and the metadata files will be created.
 
 You can bag multiple directories if you wish:
 
-    % bagit.py directory1 directory2
+    $ bagit.py directory1 directory2
 
-Optionally you can pass metadata intended for the bag-info.txt:
+Optionally you can provide metadata which will be stored in bag-info.txt:
 
-    % bagit.py --source-organization "Library of Congress" directory
+    $ bagit.py --source-organization "Library of Congress" directory
 
+You can also select which manifest algorithms will be used:
+
+    $ bagit.py --sha1 --md5 --sha256 --sha512 directory
+
+
+Using BagIt from your Python code:
+
+    import bagit
+    bag = bagit.make_bag('example-directory', {'Contact-Name': 'Ed Summers'})
+    print(bag.entries)
 
 For more information or to contribute to bagit-python's development, please
 visit https://github.com/LibraryOfCongress/bagit-python
